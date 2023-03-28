3be01183eb3f741a6c08c56132f4d08af66564be
godswillubah <118775901+godswillubah@users.noreply.github.com>
2023-03-28 18:30:34 +0100

    Update print_functions2.c

--- print_functions2.c
+++ print_functions2.c
@@ -3,12 +3,12 @@
 /****************** PRINT POINTER ******************/
 /**
  * print_pointer - Prints the value of a pointer variable
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
+ * @types: List a of arguments.
+ * @buffer: Buffer array to handle print.
+ * @flags:  Calculates active flags.
+ * @width: get width.
+ * @precision: Precision specification.
+ * @size: Size specifier.
  * Return: Number of chars printed.
  * written by Kebron Araya and Godswill Ubah.
  */
