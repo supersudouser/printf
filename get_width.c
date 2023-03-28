539d7ddcade9596002ffba753428726c394ec44c
godswillubah <118775901+godswillubah@users.nonreply.github.com>
2023-03-28 02:42:58 -0700

    Added get_width.c file

--- get_width.c
+++ get_width.c
@@ -1,36 +1,35 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- * Written by Kebron Araya and Godswill Ubah.
- * Return: width.
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
+ * Wriiten by Kebron Araya and Godswill ubah
  */
-int get_width(const char *format, int *i, va_list list)
+int get_flags(const char *format, int *i)
 {
-	int curr_i;
-	int width = 0;
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int j, curr_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (is_digit(format[curr_i]))
-		{
-			width *= 10;
-			width += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			width = va_arg(list, int);
-			break;
-		}
-		else
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[curr_i] == FLAGS_CH[j])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
+
+		if (FLAGS_CH[j] == 0)
 			break;
 	}
 
 	*i = curr_i - 1;
 
-	return (width);
+	return (flags);
 }
